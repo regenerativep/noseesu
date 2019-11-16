1f68b6b2b57dda9c2872c3367f6114b87e071d62
Crystal <czhu2020@spyponders.com>
2019-11-16 15:28:00 -0500

    commit

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-var pitchList = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+//var pitchList = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
 var pitchMap = {"C":0, "Db":1, "D":2, "Eb":3, "E":4, "F":5, "Gb":6, "G":7, "Ab":8, "A":9, "Bb":10, "B":11};
 
 var hearDistance = 2000;
