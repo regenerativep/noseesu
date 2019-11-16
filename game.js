2ce556bede4ca0b5597db60204a1efd1c5357bcb
nathandorson <32528988+nathandorson@users.noreply.github.com>
2019-11-16 11:11:35 -0500

    Update game.js

--- game.js
+++ game.js
@@ -8,3 +8,16 @@ function draw()
 {
     ellipse(50, 50, 80, 80);
 }
+
+function keyPressed()
+{
+    for(let i = 0; i < noteList.length; i++)
+    {
+        note = noteList[i];
+        let panning = map(ball.x, 0, width, -1.0, 1.0);
+        let sound = loadSound('pitches/'+note)
+        note.pan(panning);
+        soundFile.play();
+    }
+    
+}
