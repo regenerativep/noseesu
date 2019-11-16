30d257093d7f621261e99e61242b3aa16e06d29b
Regen <regeneralp@gmail.com>
2019-11-16 11:17:32 -0500

    loadmap

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+var hearDistance = 2000;
+var noteList = [];
+var currentTime;
 function setup()
 {
 
@@ -6,4 +9,24 @@ function setup()
 function draw()
 {
     
+}
+
+function loadMap(jsonStr)
+{
+    let jsonObj = JSON.parse(jsonStr);
+    let noteList = jsonObj["notes"];
+    for(let i = 0; i < noteList.length; i++)
+    {
+        let note = noteList[i];
+        addNote(note);
+    }
+    currentTime = 0;
+    console.log("loaded map with " + noteList.length + " notes");
+}
+function addNote(note)
+{
+    noteList.push(note);
+    noteList.sort((a,b) => {
+        return Math.sign(a.time - b.time);
+    });
 }
